94e10ebac7ff8e3f38faf836d371bf24b7fd86f1
tjddnr9 <tjddn9@jointree.co.kr>
2022-10-12 04:04:24 +0900

    5주

--- week05/DirectionControl.js
+++ week05/DirectionControl.js
@@ -9,29 +9,29 @@ var input = [];
 rl.on("line", (line) => {
     input.push(line);
 }).on("close", () => {
-    var n = Int.parseInt(input[0]);
+    var n = parseInt(input[0]);
     var move = input[1].split(" ");
 
     var x = 1, y = 1;
 
     move.map(mv => {
-        /**
-         * 시간을 단축할 수 있는 방법은? (기존 방식x)
-         * 1. LRUD 문자 인식
-         * 2-1. 조건문 최소화
-         * 2-2. 조건문 효율적 사용 (switch vs if)
-         * 3. 계산 최소화[간결화]
-         */
-        // 범위 벗어나면 아예 판단 pass?
         switch (mv) {
-            case "L":
             case "R":
-
+                if (x<n) x++;
+                break;
+            case "L":
+                if (x > 1) x--;
+                break;
+            case "U":
+                if (y > 1) y--;
+                break;
+            case "D":
+                if (y < n) y++;
+                break;
+            default:
+                break;
         }
     })
 
-
-
-
-    console.log(list);
+    console.log(x + " " + y);
 });
--- week05/RoyalKnight.js
+++ week05/RoyalKnight.js
@@ -13,5 +13,21 @@ rl.on("line", (line) => {
     var x = list[0].charCodeAt(0) - 96;
     var y = parseInt(list[1]);
 
-    console.log(x + " " + y);
+    var mvNum = [-1, 1, -2, 2];
+
+    var result = 0;
+
+    mvNum.map(a => {
+        if ((a + x) > 0) {
+            mvNum.map(b => {
+                if ((b + y) > 0 && Math.abs(a * b) == 2) {
+                    console.log(a + " " + b);
+                    result++;
+                }
+            })
+        }
+    })
+    console.log(result);
+
+
 });
